5022575050f659bd39723d8331969bd104cfadda
monicaamoon <monica.0758@students.amikom.ac.id>
2020-10-26 15:30:19 +0700

    moon

---
+++ app/src/main/java/com/example/praktikcustomview2/MovieAdapter.java
@@ -0,0 +1,43 @@
+package com.example.praktikcustomview2;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MovieAdapter extends ArrayAdapter<Movie> {
+    private Context mContext;
+    private int mResource;
+
+    public MovieAdapter(@NonNull Context context, int resource, @NonNull List<Movie> objects) {
+        super(context, resource, objects);
+        this.mContext = context;
+        this.mResource = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        String movieTitle = getItem(position).getTitle().toString();
+        String movieYear = getItem(position).getYear().toString();
+
+        LayoutInflater inflater = LayoutInflater.from(mContext);
+        convertView = inflater.inflate(mResource,parent,false);
+
+        TextView txtMovie = convertView.findViewById(R.id.txtTitle);
+        TextView txtYear = convertView.findViewById(R.id.txtYear);
+
+        txtMovie.setText(movieTitle);
+        txtYear.setText(movieYear);
+
+        return convertView;
+    }
+}
